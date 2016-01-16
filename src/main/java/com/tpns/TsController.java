8252176be266a47851e81ecfd91228dbbd25d089
Thanasis Sergouniotis <t.sergouniotis@afse.eu>
2016-01-16 16:45:08 +0200

    Initial commit

---
+++ src/main/java/com/tpns/TsController.java
@@ -0,0 +1,20 @@
+package com.tpns;
+
+import java.util.UUID;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/endpoint")
+public class TsController {
+
+	@PreAuthorize("#oauth2.hasScope('read')")
+	@RequestMapping(method = RequestMethod.GET, path = "/public")
+	public String findPublished() {
+		return UUID.randomUUID().toString();
+	}
+
+}
